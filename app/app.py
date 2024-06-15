9297b6d791a4f5b0fe38f82bcff3b5e46f144a26
HazemEl <156511160+HazemEldabaa@users.noreply.github.com>
2024-06-15 17:26:47 +0200

    debugging prompt page

--- app/app.py
+++ app/app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 from st_pages import show_pages_from_config
+import os
+import subprocess
+import platform
 
+if platform.system() != 'Windows':
+    setup_script = 'setup.sh'
+    if os.path.exists(setup_script):
+        subprocess.run(['bash', setup_script])
+        
 show_pages_from_config()
